ebd72bdedbfb1e94d8f83b886497accee19abc4a
karriem-lang <karriemlang@proton.me>
2025-02-26 17:13:56 -0500

    Football Challenge commit...

--- Football/ContentView.swift
+++ Football/ContentView.swift
@@ -10,10 +10,27 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("What is Football to you?")
+                .font(.largeTitle)
+                .foregroundStyle(.green)
+                .fontWeight(.light)
+            HStack {
+                Image(systemName: "figure.american.football")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundStyle(.blue)
+                
+                Image(systemName: "figure.australian.football")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundStyle(.indigo)
+                
+                Image(systemName: "figure.indoor.soccer")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundStyle(.purple)
+            }
+            
         }
         .padding()
     }
